013d1d8e4fb7f99915627fe1afe0f3f1422c0ada
feelingproudindianarmy <celestialh4@gmail.com>
2025-01-09 17:55:57 +0545

    first table

---
+++ mysql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Persons(
+PersonsID int,
+FirstName varchar(50),
+LastName varchar(50),
+Adress varchar(50),
+City varchar(50),
+Job  varchar(50)
+
+);
+
+INSERT INTO Persons(PersonsID,FirstName,LastName,Adress,City,Job)
+VALUES(1011,'alission','Becker','niger','bulgaria','computer.E'),
+(1981,'askers','cristensen','chad','nigeria','software developer');
+
+SELECT FirstName,LastName FROM Persons;
